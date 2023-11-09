f467687241599e7118abd7c25927889b30f36d62
hailey ham <roserealslow@gmail.com>
2023-11-09 14:16:29 +0900

    markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import React, { useState } from 'react';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Wrapper>
+        <Container>
+          <Header>
+            <h1>Check</h1>
+            <p>내보내기</p>
+          </Header>
+          <Main>
+            <CheckList>
+              <CheckListItem></CheckListItem>
+            </CheckList>
+          </Main>
+        </Container>
+      </Wrapper>
     </div>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  max-width: 1200px;
+  box-shadow: 0 0 10px black;
+`
+
+const Container = styled.div`
+  padding:2rem;
+  box-shadow: 0 0 10px gray;
+`
+
+const Header = styled.nav`
+  width:100%;
+  height:10rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`
+
+const Main = styled.main`
+  
+`
+
+const CheckList = styled.ul`
+  list-style: none;
+  padding:1rem;
+  display: flex;
+  gap:1rem;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  border: 1px solid pink;
+  border-radius: 5px;
+`
+
+const CheckListItem = styled.li`
+  width:5rem;
+  height:5rem;
+  border-radius: 10px;
+  text-align: center;
+  line-height: 4.8rem;
+`
+
+
